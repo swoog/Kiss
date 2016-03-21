f9da573ef50c6e43904ebed8113efa89729fa577
Aurélien GALTIER <aurelien.galtier@live.fr>
2016-03-21 18:02:05 +0100

    Add Func type

--- Kiss.Tests/TypeCheckerTests.fs
+++ Kiss.Tests/TypeCheckerTests.fs
@@ -6,6 +6,15 @@
 
     let statement a = Program(a)
 
+    let expectedTypeError errorMessage lines =
+        try
+            checkTypeProg (statement lines) |> ignore
+            raise(System.Exception("Expected TypeError received no exception"))
+        with
+        | TypeError(x) -> Assert.Equal(errorMessage, x)
+        | e -> raise(System.Exception("Expected TypeError received " + e.ToString()))
+
+
     [<Fact>] 
     let ``Should type is correct when check create variable``() = 
         let line = Create("variableName", Int(1))
@@ -14,16 +23,10 @@
 
     [<Fact>] 
     let ``Should type is incorrect when check create variable and assign``() = 
-        let lines = [
+        [
             Create("variableName", Int(1));
             Assign(Variable("variableName"), Float(1.0))
-        ]
-        try
-            checkTypeProg (statement lines) |> ignore
-            raise(System.Exception("Expected TypeError received no exception"))
-        with
-        | TypeError(x) -> Assert.Equal("Variable is not of type float", x)
-        | e -> raise(System.Exception("Expected TypeError received " + e.ToString()))
+        ] |> expectedTypeError "Variable is not of type float"
 
     [<Fact>] 
     let ``Should type is correct when check create variable with object``() = 
@@ -60,30 +63,17 @@
 
     [<Fact>] 
     let ``Should type is incorrect when check assign variable with object initialized with property``() = 
-        let lines = [
+        [
             Create("variableName", New([PropertySetter("Prop1", Int(1))]));
             Assign(Property(Variable("variableName"), "Prop1"), Float(2.0))
-        ]
-        try
-            checkTypeProg (statement lines) |> ignore
-            raise(System.Exception("Expected TypeError received no exception"))
-        with
-        | TypeError(x) -> Assert.Equal("Variable is not of type float", x)
-        | e -> raise(System.Exception("Expected TypeError received " + e.ToString()))
+        ] |> expectedTypeError "Variable is not of type float"
 
     [<Fact>] 
     let ``Should type is incorrect when check assign value to property of int``() = 
-        let lines = [
+        [
             Create("variableName", Int(1));
             Assign(Property(Variable("variableName"), "Prop1"), Float(2.0))
-        ]
-        try
-            checkTypeProg (statement lines) |> ignore
-            raise(System.Exception("Expected TypeError received no exception"))
-        with
-        | TypeError(x) -> Assert.Equal("int has no property Prop1", x)
-        | e -> raise(System.Exception("Expected TypeError received " + e.ToString()))
-
+        ] |> expectedTypeError "int has no property Prop1"
 
     [<Fact>] 
     let ``Should type is correct when check add with int``() = 
@@ -95,12 +85,39 @@
 
     [<Fact>] 
     let ``Should type is incorrect when check add int and float``() = 
-        let lines = [
+        [
             Create("variableName", Add(Int(1), Float(2.0)));
+        ] |> expectedTypeError "Expression at the left is int and the right is float"
+
+    [<Fact>] 
+    let ``Should type is correct when check func``() = 
+        let lines = [
+            Create("variableName", Fun([], []));
         ]
-        try
-            checkTypeProg (statement lines) |> ignore
-            raise(System.Exception("Expected TypeError received no exception"))
-        with
-        | TypeError(x) -> Assert.Equal("Expression at the left is int and the right is float", x)
-        | e -> raise(System.Exception("Expected TypeError received " + e.ToString()))
+        let isCheck = checkTypeProg (statement lines)
+        Assert.True(isCheck)
+
+    [<Fact>] 
+    let ``Should type is correct when check func return int``() = 
+        let lines = [
+            Create("variableName", Fun([], [Return(Int(1))]));
+        ]
+        let isCheck = checkTypeProg (statement lines)
+        Assert.True(isCheck)
+
+    [<Fact>] 
+    let ``Should type is correct when check func and assign new func``() = 
+        let lines = [
+            Create("variableName", Fun([], [Return(Int(1))]));
+            Assign(Variable("variableName"), Fun([], [Return(Int(2))]))
+        ]
+        let isCheck = checkTypeProg (statement lines)
+        Assert.True(isCheck)
+
+    [<Fact>] 
+    let ``Should type is incorrect when check func and assign new func of float``() = 
+        [
+            Create("variableName", Fun([], [Return(Int(1))]));
+            Assign(Variable("variableName"), Fun([], [Return(Float(2.0))]))
+        ] |> expectedTypeError "Variable is not of type func() : float"
+
--- Kiss/TypeChecker.fs
+++ Kiss/TypeChecker.fs
@@ -8,22 +8,34 @@ type EquationType =
 | TypeInt
 | TypeFloat
 | TypeVoid
+| TypeFunc of EquationType List * EquationType
 
 let newName = "exp"
 
 exception TypeError of string
 
-let typeToString t = 
+let rec typeToString t = 
     match t with
     | TypeInt -> "int"
     | TypeFloat -> "float"
     | TypeVoid -> "()"
     | Type(name, _) -> name
+    | TypeFunc(parameters, returnType) -> "func(" + (parameters |> List.map typeToString |> String.concat ",") + ") : " + (typeToString returnType)
 
 let rec getType typeAccu name = 
     match typeAccu with
     | [] -> raise(ParsingError("Error to search type of variable" + name))
     | (variableName, valueType)::l -> if variableName = name then valueType else (getType l name)
+
+let rec getPropertyTypeFromProperties properties propertyName =
+    match properties with
+    | [] -> raise(TypeError("property " + propertyName + " not found"))
+    | (name, t)::l -> if name = propertyName then t else getPropertyTypeFromProperties l propertyName
+
+let getPropertyType objectType propertyName = 
+    match objectType with
+    | Type(name, properties) -> getPropertyTypeFromProperties properties propertyName
+    | _ -> raise(TypeError((typeToString objectType) + " has no property " + propertyName))
 
 let rec checkTypeProperties properties =
     match properties with
@@ -41,24 +53,16 @@ and checkTypeExpression (a:Expression) =
                            type1 
                           else 
                            raise(TypeError("Expression at the left is " + (typeToString type1) + " and the right is " + (typeToString type2)))
+    | Fun(parameters, statements) -> let returnStatementType = (checkTypeStatements statements [] [])
+                                     in TypeFunc([], returnStatementType)
 
-let rec getPropertyTypeFromProperties properties propertyName =
-    match properties with
-    | [] -> raise(TypeError("property " + propertyName + " not found"))
-    | (name, t)::l -> if name = propertyName then t else getPropertyTypeFromProperties l propertyName
-
-let getPropertyType objectType propertyName = 
-    match objectType with
-    | Type(name, properties) -> getPropertyTypeFromProperties properties propertyName
-    | _ -> raise(TypeError((typeToString objectType) + " has no property " + propertyName))
-
-let rec checkTypeVariable (a:Variable) typeAccu propertyTypeAccu = 
+and checkTypeVariable (a:Variable) typeAccu propertyTypeAccu = 
     match a with
     | Variable(name) -> (getType typeAccu name)
     | Property(variable, propertyName) -> let objectType = checkTypeVariable variable typeAccu propertyTypeAccu
                                           in getPropertyType objectType propertyName
 
-let checkTypeStatement a typeAccu propertyTypeAccu = 
+and checkTypeStatement a typeAccu propertyTypeAccu = 
     match a with 
     | Create(name, e) -> (name, (checkTypeExpression e))::typeAccu
     | Assign(variable, e) -> let expressionType = (checkTypeExpression e)
@@ -69,14 +73,14 @@ let checkTypeStatement a typeAccu propertyTypeAccu =
                                     raise(TypeError("Variable is not of type " + (typeToString expressionType)))
     | Return(_) -> raise(ParsingError("check statement incorrect"))
 
-let rec checkTypeStatements (a:Statement list) typeAccu propertyTypeAccu = 
+and checkTypeStatements (a:Statement list) typeAccu propertyTypeAccu = 
     match a with
     | [] -> TypeVoid
     | (Return(a))::l -> checkTypeExpression a
     | a::l -> let typeAccu = checkTypeStatement a typeAccu propertyTypeAccu
                 in checkTypeStatements l typeAccu propertyTypeAccu
 
-let checkTypeProg (a:Prog) = 
+and checkTypeProg (a:Prog) = 
     match a with
     | Program(x) -> let progType = checkTypeStatements x [] []
                     in progType = TypeVoid
