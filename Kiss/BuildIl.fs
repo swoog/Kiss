387e635fc30f2c09ff3fcab9631f9e21d687807c
Aurélien GALTIER <aurelien.galtier@live.fr>
2016-04-18 22:20:27 +0200

    Add initialisation of type

--- Kiss/BuildIl.fs
+++ Kiss/BuildIl.fs
@@ -6,11 +6,16 @@ open System.Reflection.Emit
 open TypedAbstractSyntax
 open ToIl
 
+let rec findTypeConstructor classes name = 
+    match classes with
+    | [] -> raise(Exception("Not found"))
+    | (n, builder)::l -> if name = n then builder else (findTypeConstructor l name)
 
-let toType t = 
+let toType t classes = 
     match t with
     | TypeInt -> typedefof<int>
     | TypeBool -> typedefof<bool>
+    | Type(name, _) -> (findTypeConstructor classes name).GetType()
     | _ -> typedefof<int>
 
 let rec buildIl assemblyOutput a = 
@@ -20,40 +25,40 @@ and buildAssembly a =
     | Assembly(name, c) -> 
         let assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly (new AssemblyName(name), AssemblyBuilderAccess.RunAndSave)
         let moduleBuilder = assemblyBuilder.DefineDynamicModule(name, name, true)
-        in buildClasses c moduleBuilder assemblyBuilder;assemblyBuilder;
-and buildClasses c moduleBuilder assemblyBuilder= 
+        let classes = List.map (fun c -> match c with Class(name, _) -> (name, moduleBuilder.DefineType("Test." + name))) c
+        in buildClasses c moduleBuilder assemblyBuilder classes;assemblyBuilder;
+and buildClasses c moduleBuilder assemblyBuilder classes= 
     match c with
     | [] -> ()
-    | c::classes -> (buildClass c moduleBuilder assemblyBuilder); (buildClasses classes moduleBuilder assemblyBuilder)
-and buildClass c moduleBuilder assemblyBuilder= 
+    | c::l -> (buildClass c moduleBuilder assemblyBuilder classes); (buildClasses l moduleBuilder assemblyBuilder classes)
+and buildClass c moduleBuilder assemblyBuilder classes= 
     match c with 
-    | Class(name, methods) -> let typeBuilder = moduleBuilder.DefineType("Test." + name)
-                                in buildMethods methods typeBuilder assemblyBuilder; typeBuilder.CreateType() |> ignore
-and buildMethods m typeBuilder assemblyBuilder = 
+    | Class(name, methods) -> let typeBuilder = (findTypeConstructor classes name)
+                              in buildMethods methods typeBuilder assemblyBuilder classes; typeBuilder.CreateType() |> ignore
+and buildMethods m typeBuilder assemblyBuilder classes = 
     match m with
     | [] -> ()
-    | m::methods -> buildMethod m typeBuilder assemblyBuilder; buildMethods methods typeBuilder assemblyBuilder
-and buildMethod m typeBuilder assemblyBuilder = 
+    | m::methods -> buildMethod m typeBuilder assemblyBuilder classes; buildMethods methods typeBuilder assemblyBuilder classes
+and buildMethod m typeBuilder assemblyBuilder classes = 
     match m with
     | Method(parameters, variables, name, emits) -> 
         let methodBuilder = typeBuilder.DefineMethod(name, MethodAttributes.Public ||| MethodAttributes.HideBySig ||| MethodAttributes.NewSlot )
-        in buildEmitsIl emits (methodBuilder.GetILGenerator()) []
+        in buildEmitsIl emits (methodBuilder.GetILGenerator()) [] classes
     | EntryPoint(parameters, variables, name, emits) -> 
         let methodBuilder = typeBuilder.DefineMethod(name, MethodAttributes.Static ||| MethodAttributes.Public ||| MethodAttributes.HideBySig ||| MethodAttributes.NewSlot)
         let ilGenerator = methodBuilder.GetILGenerator()
-        let locals = List.map (fun (_, t, index) -> (ilGenerator.DeclareLocal(toType t), t, index)) variables 
-        in buildEmitsIl emits ilGenerator locals;assemblyBuilder.SetEntryPoint(methodBuilder)
-and buildEmitsIl e ilGenerator locals =
+        let locals = List.map (fun (_, t, index) -> (ilGenerator.DeclareLocal(toType t classes), t, index)) variables 
+        in buildEmitsIl emits ilGenerator locals classes;assemblyBuilder.SetEntryPoint(methodBuilder)
+and buildEmitsIl e ilGenerator locals classes =
     match e with
     | [] -> ()
-    | i::l -> buildEmitIl i ilGenerator locals []; buildEmitsIl l ilGenerator locals
+    | i::l -> buildEmitIl i ilGenerator locals classes; buildEmitsIl l ilGenerator locals classes
 and buildEmitIl e ilGenerator locals classes =
     match e with
     | Nop -> ilGenerator.Emit(OpCodes.Nop)
     | Ldc_I4(i) -> ilGenerator.Emit(OpCodes.Ldc_I4, i)
     | Stloc(index) -> ilGenerator.Emit(OpCodes.Stloc, ((findLocal locals index):LocalBuilder))
     | Ldloc(index) -> ilGenerator.Emit(OpCodes.Ldloc, ((findLocal locals index):LocalBuilder))
-    | Newobj(name) -> ilGenerator.Emit(OpCodes.Newobj, ((findTypeConstructor classes name):TypeBuilder))
+    | Newobj(name) -> ilGenerator.Emit(OpCodes.Newobj, ((findTypeConstructor classes name).GetConstructor([||])))
     | Add -> ilGenerator.Emit(OpCodes.Add)
     | Ret -> ilGenerator.Emit(OpCodes.Ret)
-and findTypeConstructor classes name = null
